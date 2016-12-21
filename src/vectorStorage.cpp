c1cb9eae8439f0573b73a7f760201f1941a97fd2
patrickfrey <patrickpfrey@yahoo.com>
2016-12-21 10:44:22 +0100

    fixed wrong sorting of find similar results

--- src/vectorStorage.cpp
+++ src/vectorStorage.cpp
@@ -88,7 +88,7 @@ public:
 					arma::vec resvv( m_database->readSampleVector( ri->featidx()));
 					ri->setWeight( arma::norm_dot( vv, resvv));
 				}
-				std::sort( rt.begin(), rt.end());
+				std::sort( rt.begin(), rt.end(), std::greater<int>());
 				rt.resize( maxNofResults);
 				return rt;
 			}
