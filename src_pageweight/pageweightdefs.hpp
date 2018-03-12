40bfc230e04249e3c33c8d41c70f1577b44c3b32
patrickfrey <patrickpfrey@yahoo.com>
2018-03-12 22:20:24 +0100

    changed definition of error codes in strusbase (no structure anymore)

---
+++ src_pageweight/pageweightdefs.hpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2016 Patrick P. Frey
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+/// \brief Compile switch that defines algorithm to use for page weight calculation
+
+/// STRUS_WITH_PAGERANK = 1 : Use pagerank 
+/// STRUS_WITH_PAGERANK = 0 : Use count of link references
+#define STRUS_WITH_PAGERANK 0
+
